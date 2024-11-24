21ca02ee9ac9f68a4eb5df271b8dd22aea5f5507
paatriksirbu <psirb@myuax.com>
2024-11-24 10:34:40 +0100

    Fragment list y todo lo necesario.

--- app/src/main/java/com/example/taller4/ListFragment.kt
+++ app/src/main/java/com/example/taller4/ListFragment.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.taller4.MyAdapter
 
 class ListFragment : Fragment() {
     private lateinit var viewModel: SharedViewModel
+    private lateinit var adapter: MyAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,17 +28,34 @@ class ListFragment : Fragment() {
         recyclerView.layoutManager = LinearLayoutManager(context)
 
         // Inicializar Adapter y asignarlo al RecyclerView
-        val adapter = MyAdapter { selectedItem ->
+        adapter = MyAdapter(mutableListOf()) { selectedItem ->
             viewModel.selectItem(selectedItem)
         }
         recyclerView.adapter = adapter
 
+        viewModel = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
+
+        viewModel.items.observe(viewLifecycleOwner) { items ->
+            adapter.updateItems(items)
+        }
+
+        val editTextNewItem = view.findViewById<EditText>(R.id.editTextNewItem)
+        val buttonAddItem = view.findViewById<Button>(R.id.buttonAddItem)
+        buttonAddItem.setOnClickListener {
+            val newItem = editTextNewItem.text.toString()
+            if (newItem.isNotEmpty()) {
+                viewModel.addItem(newItem)
+                editTextNewItem.text.clear()
+            }
+        }
+
         return view
     }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         //Iniciar el ViewModel compartido
-        viewModel = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
+        viewModel = ViewModelProvider(requireActivity())[SharedViewModel::class.java]
     }
 }
--- app/src/main/java/com/example/taller4/SharedViewModel.kt
+++ app/src/main/java/com/example/taller4/SharedViewModel.kt
@@ -5,6 +5,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SharedViewModel : ViewModel() {
+
+    private val _items = MutableLiveData<MutableList<String>>(mutableListOf())
+    val items: LiveData<MutableList<String>> get() = _items
+
+    fun addItem(newItem: String) {
+        _items.value?.add(newItem)
+        _items.value = _items.value // Para notificar a los observadores
+    }
+
     private val selectedItem = MutableLiveData<String>()
 
     fun selectItem(item: String) {
