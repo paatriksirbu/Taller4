81cfa6bf1b3ffe1a2a4951cac5f4d97dc578fe39
paatriksirbu <psirb@myuax.com>
2024-11-24 10:50:25 +0100

    Actividad principal para usar los fragments. Los fragments se usan para dividir la interfaz en secciones reutilizables. Dividimos horizontalmente

--- app/src/main/java/com/example/taller4/ActividadPrincipal.kt
+++ app/src/main/java/com/example/taller4/ActividadPrincipal.kt
@@ -1,7 +1,23 @@
 package com.example.taller4
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class ActividadPrincipal : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.actividad_principal)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_list_container, ListFragment())
+                .commit()
+        }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_detail_container, DetailFragment())
+            .commit()
+
+    }
 }
